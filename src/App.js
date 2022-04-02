6798470f92df2bd862c039c2786b5b7710a8ccf9
GRUPODIGITAL\oscar.cardenas <GRUPODIGITAL\oscar.cardenas>
2022-04-02 18:13:32 -0500

    Formulario con componentes

--- src/App.js
+++ src/App.js
@@ -1,58 +1,112 @@
-import {  useFormik } from "formik";
+//formik  component
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
-const validate = (values)=>{
- 
-    const errors = {}
+const validate = (values) => {
+  const errors = {};
 
-    if (!values.name) {
-      errors.name = "Requerido"
-    }else if (values.name.length < 5){
-      errors.name = "El nombre no puede ser menor a 5 letras"
-    } 
-
-    if (!values.lastname) {
-      errors.lastname = "Requerido"
-    }else if (values.lastname.length < 5){
-      errors.lastname = "El apellido no puede ser menor a 5 letras"
-    } 
-
-    return errors;
+  if (!values.name) {
+    errors.name = "Requerido";
+  } else if (values.name.length < 5) {
+    errors.name = "El nombre no puede ser menor a 5 letras";
   }
 
+  if (!values.lastname) {
+    errors.lastname = "Requerido";
+  } else if (values.lastname.length < 5) {
+    errors.lastname = "El apellido no puede ser menor a 5 letras";
+  }
+
+  return errors;
+};
 
 function App() {
-  const formik = useFormik({
-    initialValues: {
-      name: "",
-      lastname: "",
-      email: "",
-    },
-    validate,
-    onSubmit: (values) => console.log(values),
-  });
-
   return (
-    <form onSubmit={formik.handleSubmit}>
+    <Formik
+      initialValues={{ name: "", lastname: "", email: "" }}
+      validate={validate}
+      onSubmit={(values) => console.log(values)}
+    >
+      {(formik) => (
+        <Form>
+          <label>Nombre</label>
+          <Field name="name" type="text"/>
+          <ErrorMessage name="name"/>
+          <br />
 
-      <label>Nombre</label>
-      <input type="text" {...formik.getFieldProps('name')}/>
-      {formik.touched.name && formik.errors.name ? 
-      <div> {formik.errors.name} </div> :null }
-      <br />
+          <label>Apellido</label>
+          <Field name="lastname" type="text"/>
+          <ErrorMessage name="lastname"/>
+          <br />
 
-      <label>Apellido</label>
-      <input type="text" {...formik.getFieldProps('lastname')}/>
-        {formik.touched.lastname && formik.errors.lastname ? <div> {formik.errors.lastname} </div> :null }
-      <br />
-
-      <label>Email</label>
-      <input type="text" {...formik.getFieldProps('email')}/>
-       {formik.touched.email && formik.errors.email ? 
-       <div> {formik.errors.email} </div> :null }
-      <br />
-      <button type="submit">Enviar</button>
-    </form>
+          <label>Email</label>
+          <Field name="email" type="email"/>
+          <ErrorMessage name="email"/>
+          <br />
+          <button type="submit">Enviar</button>
+        </Form>
+      )}
+    </Formik>
   );
 }
 
 export default App;
+
+//formik no component - Hook
+
+// import {  useFormik } from "formik";
+
+// const validate = (values)=>{
+
+//     const errors = {}
+
+//     if (!values.name) {
+//       errors.name = "Requerido"
+//     }else if (values.name.length < 5){
+//       errors.name = "El nombre no puede ser menor a 5 letras"
+//     }
+
+//     if (!values.lastname) {
+//       errors.lastname = "Requerido"
+//     }else if (values.lastname.length < 5){
+//       errors.lastname = "El apellido no puede ser menor a 5 letras"
+//     }
+
+//     return errors;
+//   }
+
+// function App() {
+//   const formik = useFormik({
+//     initialValues: {
+//       name: "",
+//       lastname: "",
+//       email: "",
+//     },
+//     validate,
+//     onSubmit: (values) => console.log(values),
+//   });
+
+//   return (
+//     <form onSubmit={formik.handleSubmit}>
+
+//       <label>Nombre</label>
+//       <input type="text" {...formik.getFieldProps('name')}/>
+//       {formik.touched.name && formik.errors.name ?
+//       <div> {formik.errors.name} </div> :null }
+//       <br />
+
+//       <label>Apellido</label>
+//       <input type="text" {...formik.getFieldProps('lastname')}/>
+//         {formik.touched.lastname && formik.errors.lastname ? <div> {formik.errors.lastname} </div> :null }
+//       <br />
+
+//       <label>Email</label>
+//       <input type="text" {...formik.getFieldProps('email')}/>
+//        {formik.touched.email && formik.errors.email ?
+//        <div> {formik.errors.email} </div> :null }
+//       <br />
+//       <button type="submit">Enviar</button>
+//     </form>
+//   );
+// }
+
+// export default App;
